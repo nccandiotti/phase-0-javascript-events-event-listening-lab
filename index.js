16ad3e69c8598195c799010e4d59ca45cc1c34ce
Nicole Candiotti <nccandiotti@gmail.com>
2022-01-14 23:17:30 -0500

    commit tree 509d68acccbfe3007e20b0b7801601c4159d29a0

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const input = document.getElementById('input');
+const input = document.getElementById("input")
 
 function addingEventListener() {
-  alert('I was clicked!');
-  input.addEventListener('click', addingEventListener);
+  alert("I was clicked!")
+  input.addEventListener("click", addingEventListener)
 }
 
+// document.querySelector("#input")
+// input.addEventListener("click", function () {
+//   alert("I was clicked!")
+// })
